4c648d4420400edcf930171ee91b1755cfc51f26
Zhukovsky <valentin.zhukovsky@teamaol.com>
2016-10-25 16:47:03 +0300

    Get rid of Grunt. Install gulp. Implement initial code validation tasks

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp');
+const jscs = require('gulp-jscs');
+const jscsReporter = require('gulp-jscs-stylish');
+const jshint = require('gulp-jshint');
+
+gulp.task('jshint', () =>  {
+  return gulp.src('src/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('jscs', () => {
+  return gulp.src('src/**/*.js')
+    .pipe(jscs())
+    .pipe(jscsReporter())
+    .pipe(jscs.reporter('fail'));
+});
+
+gulp.task('validate-scripts', ['jshint', 'jscs']);
