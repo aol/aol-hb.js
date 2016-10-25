4b2e49760b6b0a9b7f7e28a8651ccbf41ad64dfd
Zhukovsky <valentin.zhukovsky@teamaol.com>
2016-10-25 17:02:29 +0300

    Enable ES6 support for hshint. Clean up excludeFiles for jscs. Apply validation rules for all scripts

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,15 @@ const jscs = require('gulp-jscs');
 const jscsReporter = require('gulp-jscs-stylish');
 const jshint = require('gulp-jshint');
 
-gulp.task('jshint', () =>  {
-  return gulp.src('src/**/*.js')
+gulp.task('jshint', () => {
+  return gulp.src('*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'))
     .pipe(jshint.reporter('fail'));
 });
 
 gulp.task('jscs', () => {
-  return gulp.src('src/**/*.js')
+  return gulp.src('*.js')
     .pipe(jscs())
     .pipe(jscsReporter())
     .pipe(jscs.reporter('fail'));
