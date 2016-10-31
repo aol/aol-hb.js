ab0571e9a62881e70922fb32d154d0b56604c99d
Zhukovsky <valentin.zhukovsky@teamaol.com>
2016-10-31 12:53:36 +0300

    E2e tests. Terminate server when e2e tests are done.

--- config/tasks/tests-e2e.js
+++ config/tasks/tests-e2e.js
@@ -2,8 +2,18 @@ const gulp = require('gulp');
 const nightwatch = require('gulp-nightwatch');
 
 gulp.task('tests-e2e', ['build', 'tests-e2e-server'], () => {
-  return gulp.src('')
+  var stream = gulp.src('')
     .pipe(nightwatch({
       configFile: 'config/e2e/nightwatch.json'
     }));
+
+  var terminateE2eServer = () => {
+    process.exit();
+  };
+
+  stream.on('end', terminateE2eServer);
+
+  stream.on('error', terminateE2eServer);
+
+  return stream;
 });
