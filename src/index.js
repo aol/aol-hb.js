18b0d797f89c82769eb2de6b63a1cb6337a09109
Zhukovsky <valentin.zhukovsky@teamaol.com>
2016-11-11 12:41:43 +0300

    Remove alert fo do not block the thread

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ let url = 'http://adserver.adtechus.com/pubapi/3.0/9599.1/3675022/0' +
   'screendensity=1;kvscreenheight=1080;kvscreenwidth=1920;kvscreendensity=1;' +
   'kvviewportwidth=1920;kvviewportheight=224;misc=1478849557997';
 
-sendGetRequest(url).then((url, method, data) => {
-  alert(url + method + data);
-});
+sendGetRequest(url);
 
 window.addEventListener('load', () => {
   document.getElementById('test').textContent = utils.getAppName();
