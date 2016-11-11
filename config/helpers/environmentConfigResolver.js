3d1b25787a3997b22fa22ae70bb10bd7fe22e459
Zhukovsky <valentin.zhukovsky@teamaol.com>
2016-11-11 11:30:56 +0300

    Revert arrow functions.

--- config/helpers/environmentConfigResolver.js
+++ config/helpers/environmentConfigResolver.js
@@ -1,11 +1,11 @@
 var resolver = {
-  resolveKarmaBrowsers: () => {
+  resolveKarmaBrowsers: function() {
     return resolver.isTravisEnvironment() ? ['CHROME_TRAVIS_CI'] : ['Chrome'];
   },
   resolveNightWatchConfig: () => {
     return 'nightwatch.' + (resolver.isTravisEnvironment() ? 'config.travis' : 'config.default');
   },
-  isTravisEnvironment: () => {
+  isTravisEnvironment: function() {
     return process.env.TRAVIS;
   }
 };
