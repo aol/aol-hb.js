b2d53b095efb76980600226f2393b195975a4c56
Zhukovsky <valentin.zhukovsky@teamaol.com>
2016-11-11 15:23:25 +0300

    Rename project build result file

--- tests/e2e/index_test.js
+++ tests/e2e/index_test.js
@@ -4,7 +4,7 @@ module.exports = {
     browser.view('views/default.html')
       .waitForElementVisible('body', 5000)
       .waitForElementVisible('#test', 5000)
-      .expect.element('#test').text.to.equal('pub-api');
+      .expect.element('#test').text.to.equal('aol-hb');
 
     browser.end();
   }
--- tests/unit/indexTest.js
+++ tests/unit/indexTest.js
@@ -2,6 +2,6 @@ import utils from 'src/helpers/utils';
 
 describe('Utils tests', () => {
   it('Get app name method test', () => {
-    expect(utils.getAppName()).to.equal('pub-api');
+    expect(utils.getAppName()).to.equal('aol-hb');
   });
 });
