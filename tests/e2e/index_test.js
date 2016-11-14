f3b95da7104f2d6792e7e3a9eb75815ab72f170b
Zhukovsky <valentin.zhukovsky@teamaol.com>
2016-11-14 21:37:39 +0300

    Fix e2e test (jscs)

--- tests/e2e/index_test.js
+++ tests/e2e/index_test.js
@@ -2,7 +2,7 @@ module.exports = {
   'Demo test': (browser) => {
 
     browser.view('views/default.html')
-      .waitForElementVisible('body', 5000)
+      .waitForElementVisible('body', 5000);
 
     browser.end();
   }
