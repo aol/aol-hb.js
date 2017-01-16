02ff3ecb968ae62e58a026a9fa54a36b371e6d19
Marcio Pereira <marcio.pereira@teamaol.com>
2017-01-16 17:51:19 +0000

    Command queue, getBidResponseByAlias API

--- src/aol-hb.js
+++ src/aol-hb.js
@@ -2,7 +2,8 @@ import 'helpers/polyfills';
 import BidManager from 'bidManager';
 import RenderingManager from 'renderingManager';
 
-let globalContext = window.aolhb = {};
+let globalContext = window.aolhb = window.aolhb || {};
+globalContext.que = globalContext.que || [];
 
 globalContext.init = (bidRequestConfig, placementsConfigs) => {
   let manager = new BidManager(bidRequestConfig, placementsConfigs);
@@ -18,6 +19,10 @@ globalContext.init = (bidRequestConfig, placementsConfigs) => {
     }
   };
 
+  globalContext.getBidResponse = function (alias) {
+    return manager.getBidResponseByAlias(alias);
+  };
+
   globalContext.refreshAd = (alias) => {
     manager.refreshAd(alias);
   };
@@ -26,5 +31,28 @@ globalContext.init = (bidRequestConfig, placementsConfigs) => {
     manager.addNewAd(placementConfig);
   };
 };
+
+globalContext.que.push = function (cmd) {
+  if (typeof cmd === 'function') {
+    try {
+      cmd.call();
+    } catch (e) {
+      console.error('Error processing command :' + e.message);
+    }
+  } else {
+    console.error('Commands written into aolhb.que.push must wrapped in a function');
+  }
+};
+
+for (var i = 0; i < globalContext.que.length; i++) {
+  if (typeof globalContext.que[i].called === 'undefined') {
+    try {
+      globalContext.que[i].call();
+      globalContext.que[i].called = true;
+    } catch (e) {
+      console.error('Error processing command :', 'aol-hb.js', e);
+    }
+  }
+}
 
 export default globalContext;
