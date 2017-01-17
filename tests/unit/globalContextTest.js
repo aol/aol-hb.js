578a48e103bfa89b1bbc5c9858e220abc9c8142f
Marcio Pereira <marcio.pereira@teamaol.com>
2017-01-17 14:15:03 +0000

    unit tests, console.warn.

---
+++ tests/unit/globalContextTest.js
@@ -0,0 +1,19 @@
+import GlobalContext from 'src/aol-hb.js';
+
+describe('GlobalContext', () => {
+
+  it('should have the global variable aolhb and its properties', () => {
+    expect(GlobalContext).to.be.a('object');
+    expect(GlobalContext).to.have.property('que').that.is.a('array');
+    expect(GlobalContext).to.have.property('init').that.is.a('function');
+  });
+
+  it('Should initiate the library and set a public API', () => {
+    GlobalContext.init({}, []);
+    expect(GlobalContext).to.have.property('renderAd').that.is.a('function');
+    expect(GlobalContext).to.have.property('getBidResponse').that.is.a('function');
+    expect(GlobalContext).to.have.property('refreshAd').that.is.a('function');
+    expect(GlobalContext).to.have.property('addNewAd').that.is.a('function');
+  });
+
+});
